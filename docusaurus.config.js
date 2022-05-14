45ab18478586f66b6dc69ee0e50ea542660589b0
callumzhong <callum.zhong@gmail.com>
2022-05-14 16:18:25 +0800

    feat: add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,10 +16,10 @@ const config = {
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'callunzhong', // Usually your GitHub org/user name.
 	projectName: 'callumzhong.github.io', // Usually your repo name.
-	deploymentBranch: 'callumzhong.github.io',
-	// trailingSlash: false,
+	organizationName: 'callunzhong', // Usually your GitHub org/user name.
+	// deploymentBranch: 'callumzhong',
+	trailingSlash: false,
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
 	// to replace "en" with "zh-Hans".
