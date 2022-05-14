2d924d081b6fa42bb5f23e9e51a5ddff97a44d9f
callumzhong <callum.zhong@gmail.com>
2022-05-14 15:51:02 +0800

    feat: update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
 	title: 'Better than yesterday',
 	tagline: 'Dinosaurs are cool',
-	url: 'https://your-docusaurus-test-site.com',
+	url: 'https://callumzhong.github.io',
 	baseUrl: '/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
@@ -17,8 +17,8 @@ const config = {
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: 'callumzhong', // Usually your GitHub org/user name.
-	projectName: 'blog', // Usually your repo name.
-
+	projectName: 'callumzhong.github.io', // Usually your repo name.
+	trailingSlash: false,
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
 	// to replace "en" with "zh-Hans".
